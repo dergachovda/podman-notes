d3c3bea8bad4ae222f725fc6e37e59943250693e
Dmytro Derhachov <dergachovda@gmail.com>
2026-03-14 23:14:38 +0000

    spring boot app fix

---
+++ devcontainers/spring-boot/src/main/java/dev/derhachov/dc_test/controller/TestControler.java
@@ -0,0 +1,23 @@
+package dev.derhachov.dc_test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/test")
+public class TestControler {
+
+    @GetMapping("hello")
+    public String hello() {
+        var msg = "hello";
+        log.info("response: {}", msg);
+        return msg;
+    }
+
+}
