854ad866801b47245430c7f3c952c33b6fa0b412
Dmytro Derhachov <dergachovda@gmail.com>
2026-03-14 23:34:59 +0200

    Add Spring Boot Devcontainer example

---
+++ devcontainers/spring-boot/src/main/java/dev/derhachov/dc_test/DcTestApplication.java
@@ -0,0 +1,13 @@
+package dev.derhachov.dc_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcTestApplication.class, args);
+	}
+
+}
---
+++ devcontainers/spring-boot/src/test/java/dev/derhachov/dc_test/DcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.derhachov.dc_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
