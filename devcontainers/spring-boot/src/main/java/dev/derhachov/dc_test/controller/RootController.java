4b4d6bd8162087a5b5d7a880c96c8280021ee1af
Dmytro Dergachov <dergachovda@gmail.com>
2026-03-17 21:43:36 +0000

    root controller

---
+++ devcontainers/spring-boot/src/main/java/dev/derhachov/dc_test/controller/RootController.java
@@ -0,0 +1,14 @@
+package dev.derhachov.dc_test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+  @GetMapping("/")
+  public String root(Model model) {
+    model.addAttribute("message", "Hello, World");
+    return "index";
+  }
+}
